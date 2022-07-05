8b806bed85528dba381f10ff351e40205da625a3
Sergey Petrykin <spetr4222@gmail.com>
2022-07-05 22:06:42 +0400

    first commit

--- RemzonaProject.py
+++ RemzonaProject.py
@@ -23,14 +23,11 @@ lb_time.grid(row=0, column=0, columnspan=4, sticky='nwse')
 
 
 
-
-
-
 class Lift(Tk):
     def __init__(self, x, y, price, name = 'Lift', **kwargs):
         
         self.frame  = Frame( background='black', **kwargs)
-        self.frame.grid(row = x, column= y, sticky='nwse', padx=10, pady=10)
+        self.frame.grid(row = x, column= y, sticky='nwse', padx=5, pady=5)
 
         self.start = 0
         self.stop = 0
@@ -40,6 +37,23 @@ class Lift(Tk):
         self.lift_count = 0
         self.drill = 0
         self.drill_var = 0
+        self.x_drill = 0
+        self.grinding_machine = 0
+        self.grinding_machine_var = 0
+        self.x_grinding_machine = 0
+        self.screwdriver = 0
+        self.screwdriver_var = 0
+        self.x_screwdriver = 0
+        self.press = 0
+        self.press_var = 0
+        self.x_press = 0
+        self.fan = 0
+        self.fan_var = 0
+        self.x_fan = 0
+        self.cleaner = 0
+        self.cleaner_var = 0
+        self.x_cleaner = 0
+
 
         
         
@@ -49,8 +63,6 @@ class Lift(Tk):
 
 
 
-
-        
 
         def click_start():
             self.lb_start.config(text=time.strftime('%H : %M : %S'))
@@ -77,7 +89,8 @@ class Lift(Tk):
             self.bt_stop.config(stat = DISABLED)
             self.bt_reset.config(stat = NORMAL)
             counter()
-            self.main_count = self.lift_count + self.drill # Здесь добавляем дополнительные услуги
+            self.main_count = self.lift_count + self.drill + self.grinding_machine + self.screwdriver \
+            + self.press + self.fan + self.cleaner  # Здесь добавляем дополнительные услуги
             self.lb_main_count.config(text = f'Итого : {self.main_count} РУБ ')
 
         def counter():
@@ -94,7 +107,6 @@ class Lift(Tk):
             
 
         def reset():
-            
             self.lb_start.config(text='-- : -- : --')
             self.lb_stop.config(text='-- : -- : --')
             self.lb_finishtime.config(text='-- : -- : --')
@@ -106,96 +118,86 @@ class Lift(Tk):
             self.main_price = price
             self.drill = 0
             self.x_drill = 0
-
+            self.grinding_machine = 0
+            self.x_grinding_machine = 0
+            self.screwdriver = 0
+            self.x_screwdriver = 0
+            self.press = 0
+            self.x_press = 0
+            self.fan = 0
+            self.x_fan = 0
+            self.cleaner = 0
+            self.x_cleaner = 0
 
         def price_tools():
             self.main_price = price + self.check_tools_var.get()
 
-        self.x_drill = 0
 
         def options():
-
-            
-
-            
-
-            
             self.win = Toplevel()
             self.win.title(f'{name}')
             self.win.grab_set()
-            
-
-            
-
-            
-
-            #self.win.protocol('WM_OPEN_WINDOW', launch)
-            
-
-
             self.frame_options = Frame(self.win, bg='red')
             self.frame_options.grid(row=0, column=0, columnspan=6, rowspan=2, sticky='nwse')
 
             
-            def price_drill():
-                
+            def price_drill(): 
                 self.drill = 0
                 self.drill += self.drill_var.get()
-                print(self.drill_var.get())
+
+            def price_grinding_machine():
+                self.grinding_machine = 0
+                self.grinding_machine += self.grinding_machine_var.get()
+
+            def price_screwdriver():
+                self.screwdriver = 0
+                self.screwdriver += self.screwdriver_var.get()
+
+            def price_press(): 
+                self.press = 0
+                self.press += self.press_var.get()
+
+            def price_fan(): 
+                self.fan = 0
+                self.fan += self.fan_var.get()
+
+            def price_cleaner(): 
+                self.cleaner = 0
+                self.cleaner += self.cleaner_var.get()
+                
 
             def launch():
-                #self.check_drill.config(variable=self.drill_var.set())
                 self.drill_var.set(self.x_drill)
-                print('launch')
-
+                self.grinding_machine_var.set(self.x_grinding_machine)
+                self.screwdriver_var.set(self.x_screwdriver)
+                self.press_var.set(self.x_press)
+                self.fan_var.set(self.x_fan)
+                self.cleaner_var.set(self.x_cleaner)
+                
 
             def memory():
-                
-                print('memory')
-                #print(self.drill_var.get())
                 self.x_drill = self.drill_var.get()
-                print(self.x_drill)
+                self.x_grinding_machine = self.grinding_machine_var.get()
+                self.x_screwdriver = self.screwdriver_var.get()
+                self.x_press = self.press_var.get()
+                self.x_fan = self.fan_var.get()
+                self.x_cleaner = self.cleaner_var.get()
                 self.win.destroy()
 
             self.win.protocol('WM_DELETE_WINDOW', memory)
 
-            '''def memory():
-                #self.check_drill.config(variable=self.drill_var.set(self.drill_var.get()))
-                print('memory')
-                print(self.drill_var.get())
-                self.win.destroy()'''
 
             
-
-            
-
-            
-
-            
-                
-
-            
-
-            
-            
-            
-            '''self.win.protocol('WM_DELETE_WINDOW', memory)'''
-
-            
-
-            
-
-            
-
-           
-
-            
-
+            self.grinding_machine_var = IntVar()
             self.check_grinding_machine = Checkbutton(self.frame_options, text='Болгарка',
                         width='15',
                         height='1',
                         font=('Arial',10),
-                        anchor='w')
+                        anchor='w',
+                        command=price_grinding_machine,
+                        onvalue=50,
+                        offvalue=0,
+                        variable=self.grinding_machine_var)
             self.check_grinding_machine.grid(row=0, column=0, sticky='nwes')
 
 
@@ -211,32 +213,54 @@ class Lift(Tk):
                         variable=self.drill_var)
             self.check_drill.grid(row=0, column=1, sticky='nwes')
 
+            self.screwdriver_var = IntVar()
             self.check_screwdriver = Checkbutton(self.frame_options, text='Шуруповерт',
                         width='15',
                         height='1',
                         font=('Arial',10),
-                        anchor='w')
+                        anchor='w',
+                        command=price_screwdriver,
+                        onvalue=100,
+                        offvalue=0,
+                        variable=self.screwdriver_var)
             self.check_screwdriver.grid(row=1, column=0, sticky='nwes')
 
+            self.press_var = IntVar()
             self.check_press = Checkbutton(self.frame_options, text='Пресс',
                         width='15',
                         height='1',
                         font=('Arial',10),
-                        anchor='w')
+                        anchor='w',
+                        command=price_press,
+                        onvalue=150,
+                        offvalue=0,
+                        variable=self.press_var)
             self.check_press.grid(row=1, column=1, sticky='nwes')
 
+            self.fan_var = IntVar()
             self.check_fan = Checkbutton(self.frame_options, text='Фен',
                         width='15',
                         height='1',
                         font=('Arial',10),
-                        anchor='w')
+                        anchor='w',
+                        command=price_fan,
+                        onvalue=150,
+                        offvalue=0,
+                        variable=self.fan_var)
             self.check_fan.grid(row=2, column=0, sticky='nwes')
+        
 
+
+            self.cleaner_var = IntVar()
             self.check_cleaner = Checkbutton(self.frame_options, text='Пылесос',
                         width='15',
                         height='1',
                         font=('Arial',10),
-                        anchor='w')
+                        anchor='w',
+                        command=price_cleaner,
+                        onvalue=150,
+                        offvalue=0,
+                        variable=self.cleaner_var)
             self.check_cleaner.grid(row=2, column=1, sticky='nwes')
 
             self.check_welding_machine_auto = Checkbutton(self.frame_options, text='Сварочный полуавтомат',
@@ -267,27 +291,8 @@ class Lift(Tk):
                         anchor='w')
             self.check_tornador.grid(row=5, column=1, sticky='nwes')
 
-            print(self.drill_var.get())
-            #self.drill_var.set(self.drill_var.get())
-            #self.check_drill.config(variable=self.drill_var.set(self.drill_var.get()))
 
             launch()
-
-
-            
-
-
-        
-
-        '''def hide_show():
-            if self.frame_options.winfo_viewable():
-                self.frame_options.grid_remove()
-                print(0)
-            else:
-                self.frame_options.grid()
-                print(1)'''
-
-        
 
 
         self.lb_main = Label(self.frame, font=('Arial',12,'bold'),           
@@ -316,85 +321,6 @@ class Lift(Tk):
 
         self.options = Button(self.frame, text='Опции', command=options)
         self.options.grid(row=2, column=1, sticky='nwse')
-
-
-        '''self.frame_options = Frame(self.win, bg='red')
-        self.frame_options.grid(row=0, column=0, columnspan=6, rowspan=2, sticky='nwse')
-
-        self.frame_options.grid_remove()  # ПРЯЧЕТ ОПЦИИ ПРИ ЗАПУСКЕ
-
-
-        self.check_grinding_machine = Checkbutton(self.frame_options, text='Болгарка',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_grinding_machine.grid(row=0, column=0, sticky='nwes')
-
-        self.check_drill = Checkbutton(self.frame_options, text='Дрель',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_drill.grid(row=0, column=1, sticky='nwes')
-
-        self.check_screwdriver = Checkbutton(self.frame_options, text='Шуруповерт',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_screwdriver.grid(row=1, column=0, sticky='nwes')
-
-        self.check_press = Checkbutton(self.frame_options, text='Пресс',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_press.grid(row=1, column=1, sticky='nwes')
-
-        self.check_fan = Checkbutton(self.frame_options, text='Фен',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_fan.grid(row=2, column=0, sticky='nwes')
-
-        self.check_cleaner = Checkbutton(self.frame_options, text='Пылесос',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_cleaner.grid(row=2, column=1, sticky='nwes')
-
-        self.check_welding_machine_auto = Checkbutton(self.frame_options, text='Сварочный полуавтомат',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_welding_machine_auto.grid(row=3, column=0, columnspan=2, sticky='nwes')
-
-        self.check_welding_machine = Checkbutton(self.frame_options, text='Сварочный инвертор',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_welding_machine.grid(row=4, column=0, columnspan=2, sticky='nwes')
-
-        self.check_polishing = Checkbutton(self.frame_options, text='Полировка',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_polishing.grid(row=5, column=0, sticky='nwes')
-
-        self.check_tornador = Checkbutton(self.frame_options, text='Торнадор',
-                        width='15',
-                        height='1',
-                        font=('Arial',10),
-                        anchor='w')
-        self.check_tornador.grid(row=5, column=1, sticky='nwes')'''
-
-
 
         self.lb_start = Label(self.frame, font=('Arial',12,'bold'), 
                 bg='blue',
@@ -439,15 +365,7 @@ class Lift(Tk):
                 )
         self.lb_main_count.grid(row=6, column=0, columnspan=2, sticky='nwse')
 
-        
-
-        
-       
-
-    
-
-
-        
+               
 
     
 def LiftOne():
@@ -464,11 +382,11 @@ def LiftFour():
 
 def PitOne():
     pit_one = Lift(2, 0 , 150, name = 'Яма № 1', bg = 'white')
-    #pit_one.main_price = 150
+    
 
 def PitTwo():
     pit_two = Lift(2, 1 , 150, name = 'Яма № 2', bg = 'white')
-    #pit_two.main_price = 150
+    
 
 def PitThree():
     pit_three = Lift(2, 2, 150, name = 'Яма № 3', bg = 'white')
@@ -477,8 +395,6 @@ def PitThree():
 def PitFour():
     pit_four = Lift(2, 3, 150, name = 'Яма № 4', bg = 'white')
     #pit_four.main_price = 150
-
-
 
 
 
